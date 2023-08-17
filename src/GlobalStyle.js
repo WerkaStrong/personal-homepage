6c2f950245e78c0e91d0892aa3e061142011b9b2
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-17 19:23:08 +0200

    created GlobalStyle.js in src

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        margin: 0 auto;
+        font-family: 'Inter', sans-serif;
+        color: ${({theme}) => theme.color.mineShaft};
+        background: ${({theme}) => theme.color.whiteLilac};
+    }
+`;
