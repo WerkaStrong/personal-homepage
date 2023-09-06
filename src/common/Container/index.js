45496e8950e789978c778e0a9bce68daf08b8392
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-06 20:52:06 +0200

    created Container component

---
+++ src/common/Container/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	background: ${({theme}) => theme.color.whiteLilac};
+	width: calc(100% - 28px);
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+    align-items: center;
+
+	@media (min-width: 1226px) {
+		margin: 0 auto;
+		max-width: 1216px;
+	}
+`
