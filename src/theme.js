4cbec3774afec746cec096edfef991384015561b
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-06 20:47:19 +0200

    added new colors to theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,8 @@ export const theme = {
 	color: {
 		white: "#FFFFFF",
 		whiteLilac: "#FBFBFE",
+		cornflowerBlue: "#0366D633",
+		dodgerBlue: "#2188FF",
 		scienceBlue: "#0366D6",
 		silver: "#C4C4C4",
 		slateGray: "#6E7E91",
