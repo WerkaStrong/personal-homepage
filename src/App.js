6c9c792380268dc0f72aca713777b3d7b96ba48c
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-12 11:33:24 +0200

    added DarkModeButton in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import './App.css';
-import { Contact } from './common/Contact';
-import { Container } from './common/Container';
-import { About } from './common/Hero-about/index';
-import { Portfolio } from './common/Portfolio';
-import { Skills } from './common/Skills/index';
+import { useSelector } from 'react-redux';
+import { lightTheme, darkTheme } from './theme';
+import { Container } from "./common/Container";
+import { About } from "./common/Hero-about/index";
+import { Portfolio } from "./common/Portfolio";
+import { Skills } from "./common/Skills/index";
+import { DarkModeButton } from "./features/DarkModeButton";
+import { Contact } from "./common/Contact";
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './GlobalStyle';
+
 
 function App() {
-  const skillsTitle = "My skillset includes";
-  const futureSkillsTitle = "What I want to learn next";
-  return (
-    <>
-      <Container>
-        <About />
-        <Skills title={skillsTitle}/>
-        <Skills title={futureSkillsTitle}/>
-        <Portfolio />
-        <Contact />
-      </Container>
-    </>
-  );
+    const themeName = useSelector((state) => state.theme.value);
+    const theme = themeName === "light" ? lightTheme : darkTheme;
+    const skillsTitle = "My skillset includes";
+    const futureSkillsTitle = "What I want to learn next";
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Container>
+                <DarkModeButton />
+                <About />
+                <Skills title={skillsTitle} />
+                <Skills title={futureSkillsTitle} />
+                <Portfolio />
+                <Contact />
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
