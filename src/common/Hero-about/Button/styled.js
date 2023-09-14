6db23d14e18261e0f56eb3b0516e7ab70d80d6bf
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-14 18:14:51 +0200

    added effect in HireButton when is hover

--- src/common/Hero-about/Button/styled.js
+++ src/common/Hero-about/Button/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HireButton = styled.button`
     background: ${({ theme }) => theme.button};
-    border: 1px solid #d1d5da4d;
+    border: 1px solid #D1D5DA4D;
     border-radius: 4px;
     color: ${({ theme }) => theme.color.white};
     font-weight: 600px;
@@ -12,6 +12,11 @@ export const HireButton = styled.button`
     padding: 12px 16px;
     gap: 16px;
     width: 154px;
+
+    &:hover {
+        box-shadow: 2px 2px 0 #8cc2ff, -2px -2px 0 #8cc2ff, 2px -2px 0 #8cc2ff,
+            -2px 2px 0 #8cc2ff;
+    }
 
     @media (min-width: 768px) {
     }
