5f06f759216a7b60ef2e258542f21b0d7d4e1196
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-19 20:27:30 +0200

    canged style in Contact

--- src/common/Contact/index.js
+++ src/common/Contact/index.js
@@ -1,12 +1,20 @@
 import {
     Content,
     Description,
+    Icon,
     SocialFrame,
     Subtitle,
     Title,
 } from "./styled";
+import GitHubLogo from "../image/Github_black.svg";
+import LinkedInLogo from "../image/LinkedIN.svg";
+import LinkedInLogoWhite from "../image/LinkedINWhite.svg";
+import GitHubLogoWhite from "../image/GithubWhite.svg";
+import { useSelector } from "react-redux";
 
 export const Contact = () => {
+    const theme = useSelector((state) => state.theme.value);
+
     return (
         <Content>
             <Subtitle>Let's Talk!</Subtitle>
@@ -16,7 +24,12 @@ export const Contact = () => {
                 have a website, dashboard or mobile app in mind and need some
                 help to make your ideas come to life, feel free to conatct me
             </Description>
-            <SocialFrame />
+            <SocialFrame>
+                <Icon src={theme === "dark" ? GitHubLogoWhite : GitHubLogo} />
+                <Icon
+                    src={theme === "dark" ? LinkedInLogoWhite : LinkedInLogo}
+                />
+            </SocialFrame>
         </Content>
     );
 };
--- src/common/Contact/styled.js
+++ src/common/Contact/styled.js
@@ -1,56 +1,66 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Content= styled.div`
-	align-self: self-start;
+export const Content = styled.div`
+    align-self: self-start;
     margin: 48px 0 31px 0;
     width: 100%;
 
-	@media (min-width: 768px) {
+    @media (min-width: 768px) {
         max-width: 691px;
-		margin: 120px 0 109px 0;
-	}
-`
-export const Subtitle= styled.div`
-	color: ${({theme}) => theme.textcolor};
-	text-transform: uppercase;
-	font-size: 12px;
-	font-weight: 700;
-	
+        margin: 120px 0 109px 0;
+    }
+`;
+export const Subtitle = styled.div`
+    color: ${({ theme }) => theme.textcolor};
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 700;
 
-	@media (min-width: 768px) {
-		font-size: 12px;
-	}
-`
-export const Title= styled.header`
-	font-size: 18px;
-	font-weight: 900;
-	margin: 8px 0 16px 0;
+    @media (min-width: 768px) {
+        font-size: 12px;
+    }
+`;
+export const Title = styled.header`
+    font-size: 18px;
+    font-weight: 900;
+    margin: 8px 0 16px 0;
 
     &:hover {
-        color: ${({theme}) => theme.button};
+        color: ${({ theme }) => theme.button};
     }
 
-	@media (min-width: 768px) {
-		font-size: 32px;
-		margin: 24px 0 24px 0;
-	}
-`
-export const Description= styled.article`
-	color: ${({theme}) => theme.describe};
-	font-size: 14px;
-	font-weight: 400;
-	margin-bottom: 24px;
-	line-height: 23.8px;
+    @media (min-width: 768px) {
+        font-size: 32px;
+        margin: 24px 0 24px 0;
+    }
+`;
+export const Description = styled.article`
+    color: ${({ theme }) => theme.describe};
+    font-size: 14px;
+    font-weight: 400;
+    margin-bottom: 24px;
+    line-height: 23.8px;
 
-	@media (min-width: 768px) {
-		font-size: 18px;
-		margin-bottom: 32px;
-	}
-`
-export const SocialFrame= styled.div`
-	
+    @media (min-width: 768px) {
+        font-size: 18px;
+        margin-bottom: 32px;
+    }
+`;
+export const SocialFrame = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(32px, 1fr));
+    gap: 16px;
+    @media (min-width: 768px) {
+        grid-template-columns: repeat(auto-fill, minmax(48px, 1fr));
+        gap: 24px;
+    }
+`;
+export const Icon = styled.img`
+    width: 32px;
+    height: 32px;
 
-	@media (min-width: 768px) {
-		
-	}
-`
+    @media (min-width: 768px) {
+        width: 48px;
+        height: 48px;
+    }
+`;
