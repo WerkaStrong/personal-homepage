22dd4286e71625e96e4120589bf5b7d65f14f45d
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-09-22 18:30:04 +0200

    added animation to ToggleMode

--- src/features/DarkModeButton/index.js
+++ src/features/DarkModeButton/index.js
@@ -1,10 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Bg, Button, Image, Text, Toggle, ToggleMode } from "./styled";
 import { toggle } from "../themeSlice";
-import sun from "../../common/image/Group.svg"
-import blackSun from "../../common/image/Group_dark.svg"
+import sun from "../../common/image/Group.svg";
+import blackSun from "../../common/image/Group_dark.svg";
 import { useState } from "react";
-
 
 export const DarkModeButton = () => {
     const dispatch = useDispatch();
@@ -13,16 +12,20 @@ export const DarkModeButton = () => {
 
     const handleToggle = () => {
         setToggle(!isToggled);
-      };
-      
-      
+    };
+
     return (
         <ToggleMode>
             <Text>Dark mode {theme === "dark" ? "on" : "off"}</Text>
-            <Button onClick={() => { dispatch(toggle()); handleToggle(); }}>
+            <Button
+                onClick={() => {
+                    dispatch(toggle());
+                    handleToggle();
+                }}
+            >
                 <Bg>
                     <Toggle animate={isToggled}>
-                        <Image src={theme === "dark"? blackSun : sun } />
+                        <Image src={theme === "dark" ? blackSun : sun} />
                     </Toggle>
                 </Bg>
             </Button>
