b80dc2a871feb05b247a5e60bbf14828919febdb
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-10-08 16:12:08 +0200

    added description to About

--- src/common/Hero-about/index.js
+++ src/common/Hero-about/index.js
@@ -1,22 +1,32 @@
-import { Button } from "./Button"
-import { Picture, Tile, Title, Description, Subtitle, PictureBox, Content } from "./styled"
+import { Button } from "./Button";
+import {
+    Picture,
+    Tile,
+    Title,
+    Description,
+    Subtitle,
+    PictureBox,
+    Content,
+} from "./styled";
+import profile from "../image/linkedIn.jpg";
 
 export const About = () => {
-
     return (
-            <Tile>
-                <PictureBox>
-                    <Picture>
-                    </Picture>
-                </PictureBox>
-                <Content>
-                    <Subtitle>This is</Subtitle>
-                    <Title>Weronika Chmielewska</Title>
-                    <Description>
-                        👨🏻‍💻  Im a passionate Frontend Developer in love with React,currently looking for new job opportunities.
-                    </Description>
-                    <Button />
-                </Content>
-            </Tile>
-    )
-}
+        <Tile>
+            <PictureBox>
+                <Picture src={profile} />
+            </PictureBox>
+            <Content>
+                <Subtitle>This is</Subtitle>
+                <Title>Weronika Chmielewska</Title>
+                <Description>
+                    👩‍💻 I'm a passionate front-end developer, deeply fascinated by
+                    React and committed to creating captivating user
+                    experiences. I am actively seeking new and exciting job
+                    opportunities to continue my web development journey.
+                </Description>
+                <Button />
+            </Content>
+        </Tile>
+    );
+};
