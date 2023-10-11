7e7e7590e96ff0fada420deea0e813a34a19f5fe
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-10-11 17:21:31 +0200

    deleted unnecessary file

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import themeReducer from '../features/themeSlice'
-
-export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
-})
